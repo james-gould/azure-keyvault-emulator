12cf8fd7f5db5721f9b02d824b6c14a63e846a50
James Gould <hello@tanz.dev>
2025-03-15 12:08:53 +0000

    Updated services to make use of nullable context and added ref to EC key encryption support

--- AzureKeyVaultEmulator/ServiceConfiguration/ServiceRegistration.cs
+++ AzureKeyVaultEmulator/ServiceConfiguration/ServiceRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using AzureKeyVaultEmulator.Keys.Services;
 using AzureKeyVaultEmulator.Secrets.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AzureKeyVaultEmulator.ServiceConfiguration
@@ -8,6 +9,7 @@ namespace AzureKeyVaultEmulator.ServiceConfiguration
     {
         public static IServiceCollection RegisterCustomServices(this IServiceCollection services)
         {
+            services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IKeyVaultKeyService, KeyVaultKeyService>();
             services.AddScoped<IKeyVaultSecretService, KeyVaultSecretService>();
 
