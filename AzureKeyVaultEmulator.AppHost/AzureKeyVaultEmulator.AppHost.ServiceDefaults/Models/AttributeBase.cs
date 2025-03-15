e9f3efec714ad94a539aa4bc3a8ba77dc4d5baa0
James Gould <hello@tanz.dev>
2025-03-15 11:36:48 +0000

    Added .NET Aspire hosting to allow for easier integration testing

---
+++ AzureKeyVaultEmulator.AppHost/AzureKeyVaultEmulator.AppHost.ServiceDefaults/Models/AttributeBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AzureKeyVaultEmulator.Shared.Models
+{
+    public class AttributeBase
+    {
+        [JsonPropertyName("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonPropertyName("exp")]
+        public int Expiration { get; set; }
+
+        [JsonPropertyName("nbf")]
+        public int NotBefore { get; set; }
+    }
+}
