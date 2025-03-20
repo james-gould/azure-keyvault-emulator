cf25eb9a16289b681afc8ca373474c532e9ef3e3
James Gould <hello@tanz.dev>
2025-03-20 22:46:33 +0000

    Corrected interface to marry up to service implementation

--- AzureKeyVaultEmulator/Secrets/Services/IKeyVaultSecretService.cs
+++ AzureKeyVaultEmulator/Secrets/Services/IKeyVaultSecretService.cs
@@ -10,11 +10,11 @@ namespace AzureKeyVaultEmulator.Secrets.Services
         BackupSecretResult? BackupSecret(string name);
         SecretResponse? GetDeletedSecret(string name);
         ListResult<SecretResponse> GetDeletedSecrets(int maxVersions = 25, int skipCount = 0);
-        IEnumerable<string> GetSecretVersions(string secretName, int maxResults = 25, int skipCount = 0);
-        IEnumerable<SecretResponse?> GetSecrets(int maxResults = 25, int skipCount = 0);
+        ListResult<SecretResponse> GetSecretVersions(string secretName, int maxResults = 25, int skipCount = 0);
+        ListResult<SecretResponse> GetSecrets(int maxResults = 25, int skipCount = 0);
         void PurgeDeletedSecret(string name);
         void RecoverDeletedSecret(string name);
-        BackupSecretResult? RestoreSecret(string encodedName);
+        SecretResponse? RestoreSecret(string encodedName);
         void UpdateSecret(string name, string version, SecretAttributesModel? attributes = null, string contentType = "", object? tags = null);
     }
 }
