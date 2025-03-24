8a495d9ba622a63295909eb02c7ffa2d76c2f919
James Gould <hello@jamesgould.dev>
2025-03-24 16:25:56 +0000

    script corrected and gitignore updated

--- local-certs/makecert.ps1
+++ local-certs/makecert.ps1
@@ -1,9 +1,9 @@
 write-host "Creating .crt and .pfx to be used in emulator container"
 
-dotnet dev-certs https -eq ./emulator.crt -p emulator
+dotnet dev-certs https -ep ./emulator.crt -p emulator
 
 write-host "Created emulator.crt with password emulator"
 
-dotnet dev-certs https -eq ./emulator.pfx -p emulator
+dotnet dev-certs https -ep ./emulator.pfx -p emulator
 
 write-host "Created emulator.pfx with password emulator"
