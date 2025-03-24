376ba4589693534c9e51fd7c196d08c3d7c53ae0
James Gould <hello@jamesgould.dev>
2025-03-24 16:22:59 +0000

    Corrected cert script

--- local-certs/makecert.ps1
+++ local-certs/makecert.ps1
@@ -1,29 +1,9 @@
-write-host "Creating .crt and .key for emulator.vault.azure.net"
+write-host "Creating .crt and .pfx to be used in emulator container"
 
-openssl genpkey -algorithm RSA -out emulator.key -aes256
+dotnet dev-certs https -eq ./emulator.crt -p emulator
 
-# openssl req `
-#     -x509 `
-#     -newkey rsa:4096 `
-#     -sha256 `
-#     -days 3560 `
-#     -nodes `
-#     -keyout emulator.key `
-#     -out emulator.crt `
-#     -subj '/CN=emulator.azure.vault.net' `
-#     -extensions san `
-#     -config config.txt
+write-host "Created emulator.crt with password emulator"
 
-openssl req -new -key emulator.key -out emulator.csr -config config.txt
+dotnet dev-certs https -eq ./emulator.pfx -p emulator
 
-openssl x509 -req -in emulator.csr -signkey emulator.key -out emulator.crt -days 3650 -extensions v3_req -extfile config.txt
-
-
-
-write-host "Creating PFX certificate to be used in Docker container"
-
-write-host "You will be prompted to create a password, this is required inside the Dockerfile!"
-
-# openssl pkcs12 -export -out emulator.pfx -inkey emulator.key -in emulator.crt
-
-openssl pkcs12 -export -out emulator.pfx -inkey emulator.key -in emulator.crt
+write-host "Created emulator.pfx with password emulator"
