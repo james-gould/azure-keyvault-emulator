561e2416e8b9cf1a24caf9d9f35e5e721734254b
James Gould <hello@jamesgould.dev>
2025-04-12 22:33:16 +0100

    Refactor

--- AzureKeyVaultEmulator.Shared/Models/Keys/KeyReleaseVM.cs
+++ AzureKeyVaultEmulator.Shared/Models/Keys/KeyReleaseVM.cs
@@ -9,24 +9,24 @@ public sealed class KeyReleaseVM(string aas)
     public string AasEhd { get; set; } = aas;
 
     [JsonPropertyName("iss")]
-    public string Issuer => $"{AuthConstants.EmulatorUri}/keys";
+    public static string Issuer => $"{AuthConstants.EmulatorUri}/keys";
 
     [JsonPropertyName("sgx-mrenclave")]
-    public string SGXEnclave => "0000000000000000000000000000000000000000000000000000000000000000";
+    public static string SGXEnclave => "0000000000000000000000000000000000000000000000000000000000000000";
 
     [JsonPropertyName("sgx-mrsigner")]
-    public string SGXMrsigner => "86788fe40448f2a12e20bf8d5e7a1c3139bc5fdc1432b370c1da3489ab649a85";
+    public static string SGXMrsigner => "86788fe40448f2a12e20bf8d5e7a1c3139bc5fdc1432b370c1da3489ab649a85";
 
     [JsonPropertyName("is-debuggable")]
-    public bool Debuggable => true;
+    public static bool Debuggable => true;
 
     [JsonPropertyName("tee")]
-    public string Tee => "sgx";
+    public static string Tee => "sgx";
 
     [JsonPropertyName("iat")]
-    public long IssuedAt => DateTimeOffset.Now.ToUnixTimeSeconds();
+    public static long IssuedAt => DateTimeOffset.Now.ToUnixTimeSeconds();
 
     [JsonPropertyName("exp")]
-    public long Expiry => DateTimeOffset.Now.AddDays(31).ToUnixTimeSeconds();
+    public static long Expiry => DateTimeOffset.Now.AddDays(31).ToUnixTimeSeconds();
 
 }
