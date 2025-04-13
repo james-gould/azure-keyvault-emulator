8a3e38c8208b5ee5c9d74f3c33b7ec73345607c6
James Gould <hello@jamesgould.dev>
2025-04-13 16:36:08 +0100

    Fixed AppHost issue cloaking where the emulator API source is

--- AzureKeyVaultEmulator.AppHost/AzureKeyVaultEmulator.AppHost.AppHost/Program.cs
+++ AzureKeyVaultEmulator.AppHost/AzureKeyVaultEmulator.AppHost.AppHost/Program.cs
@@ -3,16 +3,16 @@ using AzureKeyVaultEmulator.Shared.Constants;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var useEmulatorContainer = bool.Parse(Environment.GetEnvironmentVariable("UseEmulator") ?? "false");
-var keyVaultServiceName = "keyvault";
+//var useEmulatorContainer = bool.Parse(Environment.GetEnvironmentVariable("UseEmulator") ?? "false");
+var useDeployedDockerContainer = false;
 
-if (useEmulatorContainer)
+if (useDeployedDockerContainer)
 {
     //var keyVault = builder
     //    .AddAzureKeyVault(keyVaultServiceName)
     //    .RunAsEmulator();
 
-    var keyVault = builder.AddAzureKeyVaultEmulator(keyVaultServiceName);
+    var keyVault = builder.AddAzureKeyVaultEmulator(AspireConstants.EmulatorServiceName);
 
     var webApi = builder
         .AddProject<Projects.WebApiWithEmulator_DebugHelper>("sampleApi")
@@ -21,7 +21,12 @@ if (useEmulatorContainer)
 }
 else
 {
-    builder.AddProject<Projects.AzureKeyVaultEmulator>(AspireConstants.EmulatorServiceName);
+    var keyVault = builder.AddProject<Projects.AzureKeyVaultEmulator>(AspireConstants.EmulatorServiceName);
+
+builder
+    .AddProject<Projects.WebApiWithEmulator_DebugHelper>("sampleApi")
+    .WithReference(keyVault)
+    .WaitFor(keyVault);
 }
 
 builder.Build().Run();
