220aade9972a7e6bcdcb079a99a3f2bf0a58cec5
BaturhanKahraman <baturhan.kahraman@gmail.com>
2025-04-22 13:50:14 +0200

    Nullable types refactored

--- AzureKeyVaultEmulator/Secrets/Services/ISecretService.cs
+++ AzureKeyVaultEmulator/Secrets/Services/ISecretService.cs
@@ -4,17 +4,17 @@ namespace AzureKeyVaultEmulator.Secrets.Services
 {
     public interface ISecretService
     {
-        SecretResponse? Get(string name, string version = "");
-        SecretResponse? SetSecret(string name, SetSecretModel requestBody);
-        DeletedSecretBundle? DeleteSecret(string name, string version = "");
-        ValueModel<string>? BackupSecret(string name);
-        SecretResponse? GetDeletedSecret(string name);
+        SecretResponse Get(string name, string version = "");
+        SecretResponse SetSecret(string name, SetSecretModel requestBody);
+        DeletedSecretBundle DeleteSecret(string name, string version = "");
+        ValueModel<string> BackupSecret(string name);
+        SecretResponse GetDeletedSecret(string name);
         ListResult<SecretResponse> GetDeletedSecrets(int maxVersions = 25, int skipCount = 0);
         ListResult<SecretResponse> GetSecretVersions(string secretName, int maxResults = 25, int skipCount = 0);
         ListResult<SecretResponse> GetSecrets(int maxResults = 25, int skipCount = 0);
         void PurgeDeletedSecret(string name);
-        SecretResponse? RecoverDeletedSecret(string name);
-        SecretResponse? RestoreSecret(string encodedName);
+        SecretResponse RecoverDeletedSecret(string name);
+        SecretResponse RestoreSecret(string encodedName);
         SecretAttributesModel UpdateSecret(string name, string version, SecretAttributesModel attributes);
     }
 }
