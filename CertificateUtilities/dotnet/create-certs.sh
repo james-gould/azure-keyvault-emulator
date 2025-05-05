942b5a911b0223f69850af2a83bc5214910350ff
James Gould <hello@jamesgould.dev>
2025-05-05 12:15:40 +0100

    Removed duplicates files

--- CertificateUtilities/dotnet/create-certs.sh
+++
@@ -1,9 +0,0 @@
-echo Creating .crt and .pfx to be used in emulator container
-
-dotnet dev-certs https -ep local-certs/emulator.crt -p emulator -q
-
-echo Created emulator.crt with password emulator
-
-dotnet dev-certs https -ep local-certs/emulator.pfx -p emulator -q
-
-echo Created emulator.pfx with password emulator. You must now install these certificates locally as a Trusted Root CA Authority.
