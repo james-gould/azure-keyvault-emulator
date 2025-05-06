3fc110edf6809ba87761098f3ae8d0ac7cad40c7
James Gould <james-gould@users.noreply.github.com>
2025-05-06 16:59:49 +0100

    Fixed certificate generation scripts to work on WSL and Linux

--- docs/CertificateUtilities/openssl/create-certs.sh
+++ docs/CertificateUtilities/openssl/create-certs.sh
@@ -1,8 +1,24 @@
+#!/bin/sh
 echo Creating certificates using openssl
+
+CONFIG=$(cat <<EOF
+[req]
+distinguished_name = req_distinguished_name
+x509_extensions = san
+prompt = no
+
+[req_distinguished_name]
+CN = localhost
+
+[san]
+subjectAltName = DNS:localhost, DNS:emulator, DNS:localhost.vault.azure.net
+EOF
+)
 
 openssl req \
 -x509 \
 -newkey rsa:4096 \
+-passin pass:emulator \
 -sha256 \
 -days 3560 \
 -nodes \
@@ -11,15 +27,10 @@ openssl req \
 -subj '/CN=localhost' \
 -extensions san \
 -config <( \
-    echo '[req]'; \
-    echo 'distinguished_name=req'; \
-    echo '[san]'; \
-    echo 'subjectAltName=DNS.1:localhost,DNS.2:emulator,DNS.3:localhost.vault.azure.net')
+    echo -n "$CONFIG")
 
 echo Exported emulator.key and emulator.crt. Now creating emulator.pfx
 
-openssl pkcs12 -export -out emulator>.pfx \
--inkey emulator>.key \
--in emulator>.crt
+openssl pkcs12 -export -out emulator.pfx -inkey emulator.key -in emulator.crt -passin pass:emulator -passout pass:emulator -name "Azure Key Vault Emulator"
 
-echo PFX create, you must now install these as a Trusted Root CA Authority.
+echo PFX created with password: emulator, you must now install these as a Trusted Root CA Authority.
