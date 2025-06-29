b3943431ebb59e8ec7acadd890f84ee335ec60ca
James Gould <james-gould@users.noreply.github.com>
2025-06-29 13:13:17 +0100

    Wrote test to ensure data is persisted between container runs, mirroring usage in a fixture

--- test/AzureKeyVaultEmulator.TestContainers.Tests/AzureKeyVaultEmulatorContainerIntegrationTests.cs
+++ test/AzureKeyVaultEmulator.TestContainers.Tests/AzureKeyVaultEmulatorContainerIntegrationTests.cs
@@ -3,6 +3,7 @@ using AzureKeyVaultEmulator.TestContainers.Helpers;
 using AzureKeyVaultEmulator.TestContainers.Constants;
 using Azure.Security.KeyVault.Certificates;
 using Azure.Security.KeyVault.Keys;
+using Azure;
 
 namespace AzureKeyVaultEmulator.TestContainers.Tests;
 
@@ -94,5 +95,48 @@ public class AzureKeyVaultEmulatorContainerIntegrationTests
         var fromStore = await client.GetKeyAsync(keyName);
 
         Assert.Equal(keyName, fromStore.Value.Name);
+    }
+
+    [Fact]
+    public async Task CreatingSetupDatabaseWillPersistBetweenRuns()
+    {
+        var secretName = Guid.NewGuid().ToString();
+        var secretValue = Guid.NewGuid().ToString();
+
+        // Mark with -e Persist=true to create an SQLite Database
+        await using var container = new AzureKeyVaultEmulatorContainer(persist: true);
+
+        await container.StartAsync();
+
+        var setupClient = container.GetSecretClient();
+
+        await Assert.ThrowsAsync<RequestFailedException>(() => setupClient.GetSecretAsync(secretName));
+
+        var setupSecret = await setupClient.SetSecretAsync(secretName, secretValue);
+
+        Assert.Equal(secretName, setupSecret.Value.Name);
+        Assert.Equal(secretValue, setupSecret.Value.Value);
+
+        var fromStoreAfterSetup = await setupClient.GetSecretAsync(secretName);
+
+        Assert.Equal(secretName, fromStoreAfterSetup.Value.Name);
+        Assert.Equal(secretValue, fromStoreAfterSetup.Value.Value);
+
+        // Kill the setup container
+        await container.StopAsync();
+
+        // Create another with -e Persist=true so it uses the existing SQLite Database.
+        await using var secondaryContainer = new AzureKeyVaultEmulatorContainer(persist: true);
+
+        await secondaryContainer.StartAsync();
+
+        // Get secondary client, acting as if this is done within a test class using a fixture
+        var secondaryClient = secondaryContainer.GetSecretClient();
+
+        var secretFromSecondaryStore = await secondaryClient.GetSecretAsync(secretName);
+
+        Assert.Equal(secretName, secretFromSecondaryStore.Value.Name);
+        Assert.Equal(secretValue, secretFromSecondaryStore.Value.Value);
+        Assert.Equal(fromStoreAfterSetup.Value.Value, secretFromSecondaryStore.Value.Value);
     }
 }
