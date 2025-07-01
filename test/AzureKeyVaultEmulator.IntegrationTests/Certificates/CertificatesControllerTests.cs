0c2a1dfe9c73b307b97293eb0e1c07c79ea059b9
GABRIELNGBTUC <132353744+GABRIELNGBTUC@users.noreply.github.com>
2025-07-01 16:38:47 +0200

    UpdateCertificate now matches API Schema (#262)
    * Bump Azure.Identity and 6 others (#261)
    
    Bumps Azure.Identity to 1.14.1
    Bumps Azure.Security.KeyVault.Keys to 4.8.0
    Bumps coverlet.collector from 6.0.2 to 6.0.4
    Bumps Microsoft.NET.Test.Sdk from 17.10.0 to 17.14.1
    Bumps Testcontainers from 4.0.0 to 4.6.0
    Bumps xunit from 2.7.0 to 2.9.3
    Bumps xunit.runner.visualstudio from 2.5.7 to 3.1.1
    
    ---
    updated-dependencies:
    - dependency-name: Azure.Identity
      dependency-version: 1.14.1
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: Azure.Identity
      dependency-version: 1.14.1
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: Azure.Identity
      dependency-version: 1.14.1
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: Azure.Security.KeyVault.Keys
      dependency-version: 4.8.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: Azure.Security.KeyVault.Keys
      dependency-version: 4.8.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: coverlet.collector
      dependency-version: 6.0.4
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: Microsoft.NET.Test.Sdk
      dependency-version: 17.14.1
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: Testcontainers
      dependency-version: 4.6.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: xunit
      dependency-version: 2.9.3
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: xunit.runner.visualstudio
      dependency-version: 3.1.1
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Allow to update specific certificate versions
    
    * Update certificate properties update endpoint to be aligned with the documentation https://learn.microsoft.com/en-us/rest/api/keyvault/certificates/update-certificate/update-certificate?view=rest-keyvault-certificates-7.4&tabs=HTTP
    
    * Added test to validate that the certificate properties correctly populate the "Version" properties used to determine the version of the certificate to update
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- test/AzureKeyVaultEmulator.IntegrationTests/Certificates/CertificatesControllerTests.cs
+++ test/AzureKeyVaultEmulator.IntegrationTests/Certificates/CertificatesControllerTests.cs
@@ -255,6 +255,54 @@ public class CertificatesControllerTests(CertificatesTestingFixture fixture)
     }
 
     [Fact]
+    public async Task UpdatingCertificatePropertiesWithVersionWillPersistChange()
+    {
+        var client = await fixture.GetClientAsync();
+
+        var certName = fixture.FreshlyGeneratedGuid;
+
+        await fixture.CreateCertificateAsync(certName);
+
+        var fromStore = await client.GetCertAsync(certName);
+
+        Assert.NotNull(fromStore);
+        Assert.NotNull(fromStore.Properties);
+
+        var updatedProperties = new CertificateProperties(fromStore.Id)
+        {
+            Enabled = false
+        };
+
+        var response = await client.UpdateCertificatePropertiesAsync(updatedProperties);
+
+        Assert.NotNull(response.Value);
+
+        var updatedFromStore = await client.GetCertAsync(certName);
+
+        Assert.Equal(updatedProperties.Enabled, updatedFromStore.Properties.Enabled);
+    }
+
+    [Fact]
+    public async Task UriWIllUpdateCertificatePropertiesVersion()
+    {
+        var client = await fixture.GetClientAsync();
+
+        var certName = fixture.FreshlyGeneratedGuid;
+
+        await fixture.CreateCertificateAsync(certName);
+
+        var fromStore = await client.GetCertAsync(certName);
+
+        Assert.NotNull(fromStore);
+        Assert.NotNull(fromStore.Properties);
+
+        var updatedProperties = new CertificateProperties(fromStore.Id);
+
+        Assert.False(string.IsNullOrEmpty(updatedProperties.Version));
+        Assert.Equal(fromStore.Properties.Version, updatedProperties.Version);
+    }
+
+    [Fact]
     public async Task GetCertificatePolicyWillSucceed()
     {
         var client = await fixture.GetClientAsync();
