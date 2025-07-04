f022a67e708e44c5b87f594a1aac82a839bba6b6
James Gould <james-gould@users.noreply.github.com>
2025-07-04 20:03:41 +0100

    Updated AppHost to use container image instead of API, ensuring the container port always allocates dynamically

--- dev/AzureKeyVaultEmulator.AppHost/AzureKeyVaultEmulator.AppHost.AppHost/Program.cs
+++ dev/AzureKeyVaultEmulator.AppHost/AzureKeyVaultEmulator.AppHost.AppHost/Program.cs
@@ -1,38 +1,17 @@
 using AzureKeyVaultEmulator.Aspire.Hosting;
 using AzureKeyVaultEmulator.Shared.Constants;
-using AzureKeyVaultEmulator.Shared.Utilities;
 
 var builder = DistributedApplication.CreateBuilder();
 
-// Horrendous bodge for integration testing but doing a full RootCommand pattern here for 1 arg feels... overkill;
-var integrationTestRun = args.Where(x => x.Equals("--test")).FirstOrDefault() is not null;
+var keyVault = builder
+    .AddAzureKeyVault(AspireConstants.EmulatorServiceName)
+    .RunAsEmulator(
+    //new KeyVaultEmulatorOptions { Lifetime = ContainerLifetime.Persistent }
+    );
 
-var overrideTestRun = EnvUtils.GetFlag("Override");
-var persist = EnvUtils.GetFlag(EnvironmentConstants.UsePersistedDataStore);
-
-if (integrationTestRun || overrideTestRun)
-{
-    var keyVault = builder
-        .AddAzureKeyVault(AspireConstants.EmulatorServiceName)
-        .RunAsEmulator(
-            //new KeyVaultEmulatorOptions { Lifetime = ContainerLifetime.Persistent }
-        );
-
-    var webApi = builder
-        .AddProject<Projects.WebApiWithEmulator_DebugHelper>("sampleApi")
-        .WithReference(keyVault)
-        .WaitFor(keyVault);
-}
-else
-{
-    var keyVault = builder
-        .AddAzureKeyVault(AspireConstants.EmulatorServiceName)
-        .RunAsEmulator();
-
-    var webApi = builder
-        .AddProject<Projects.WebApiWithEmulator_DebugHelper>("sampleApi")
-        .WithReference(keyVault)
-        .WaitFor(keyVault);
-}
+var webApi = builder
+    .AddProject<Projects.WebApiWithEmulator_DebugHelper>("sampleApi")
+    .WithReference(keyVault)
+    .WaitFor(keyVault);
 
 builder.Build().Run();
