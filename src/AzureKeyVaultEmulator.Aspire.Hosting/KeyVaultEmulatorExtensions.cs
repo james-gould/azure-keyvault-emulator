1d90b60ac55cdf23c696a2f049572aae81f337b8
David Fowler <davidfowl@gmail.com>
2025-08-09 23:32:29 -0400

    Refactor using directives in KeyVaultEmulatorExtensions and KeyVaultEmulatorOptions for improved organization

--- src/AzureKeyVaultEmulator.Aspire.Hosting/KeyVaultEmulatorExtensions.cs
+++ src/AzureKeyVaultEmulator.Aspire.Hosting/KeyVaultEmulatorExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Aspire.Hosting.Azure;
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Azure;
 using AzureKeyVaultEmulator.Aspire.Hosting.Constants;
 using AzureKeyVaultEmulator.Aspire.Hosting.Exceptions;
 using AzureKeyVaultEmulator.Aspire.Hosting.Helpers;
--- src/AzureKeyVaultEmulator.Aspire.Hosting/KeyVaultEmulatorOptions.cs
+++ src/AzureKeyVaultEmulator.Aspire.Hosting/KeyVaultEmulatorOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using Aspire.Hosting.ApplicationModel;
 
 namespace AzureKeyVaultEmulator.Aspire.Hosting;
 
