8034106a691eb98513b2819d96821dc9dc4162d3
James Gould <james-gould@users.noreply.github.com>
2025-08-19 22:55:38 +0100

    Corrected tag for updated release

--- src/AzureKeyVaultEmulator.Aspire.Hosting/Constants/KeyVaultEmulatorContainerConstants.cs
+++ src/AzureKeyVaultEmulator.Aspire.Hosting/Constants/KeyVaultEmulatorContainerConstants.cs
@@ -8,7 +8,7 @@
         public const string Image = "jamesgoulddev/azure-keyvault-emulator";
         public const int Port = 4997;
 
-        public const string Tag = "2.5.5";
+        public const string Tag = "2.5.6";
 
     }
 
