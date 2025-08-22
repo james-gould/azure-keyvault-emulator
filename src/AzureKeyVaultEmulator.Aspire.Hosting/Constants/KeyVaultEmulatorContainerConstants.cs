7a67cdde8cbafcf13599b57cf769afd3e50aa461
James Gould <james-gould@users.noreply.github.com>
2025-08-22 16:13:25 +0100

    Versions bumped to base increment tag

--- src/AzureKeyVaultEmulator.Aspire.Hosting/Constants/KeyVaultEmulatorContainerConstants.cs
+++ src/AzureKeyVaultEmulator.Aspire.Hosting/Constants/KeyVaultEmulatorContainerConstants.cs
@@ -8,7 +8,7 @@
         public const string Image = "jamesgoulddev/azure-keyvault-emulator";
         public const int Port = 4997;
 
-        public const string Tag = "2.5.7";
+        public const string Tag = "2.5.8";
         public static string ArmTag => $"{Tag}-arm";
 
     }
